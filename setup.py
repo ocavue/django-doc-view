865c85213dd3017aa6059e34103746cbd0e26581
ocavue <ocavue@MBP.no_rep>
2018-04-09 11:59:09 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_doc_view',
-    version='0.1.0',
+    version='0.1.1',
     keywords=('django', 'api', 'view', 'document', 'doc'),
     description='The simplest way to document your Django APIs',
     author='Ocavue',
