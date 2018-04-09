2893bc1afb40ce931f0846409a0b522c7d6740f1
ocavue <ocavue@MBP.no_rep>
2018-04-09 11:58:47 +0800

    add arguments -f --format for custom format

--- django_doc_view/management/commands/doc_view.py
+++ django_doc_view/management/commands/doc_view.py
@@ -4,14 +4,31 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from django_doc_view import django_doc_view
+from django_doc_view.core import DEFAULT_OUTPUT_FORMAT
 
 
 class Command(BaseCommand):
-    help = 'document'  # TODO
+    help = 'document django project. read this page for more info: https://github.com/ocavue/django-doc-view'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-f',
+            '--format',
+            required=False,
+            type=str,
+            help='use custom format to replace default format: "{}"'.format(
+                DEFAULT_OUTPUT_FORMAT.replace('\n', '\\n')
+            ),
+        )
 
     def handle(self, *args, **options):
         urlpatterns = importlib.import_module(
             settings.ROOT_URLCONF
         ).urlpatterns
-        result = django_doc_view(urlpatterns)
+        if options.get('format'):
+            result = django_doc_view(
+                urlpatterns, output_format=options['format'].replace('\\n', '\n')
+            )
+        else:
+            result = django_doc_view(urlpatterns)
         print(result)
